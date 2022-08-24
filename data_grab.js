8b5b985e6d12f62f4c5ae6875e2d118fa5c2c012
Matthew Thomas Dressa <mtd67@cornell.edu>
2022-08-24 17:08:32 -0500

    updated form handling

--- data_grab.js
+++ data_grab.js
@@ -1,14 +1,20 @@
+// JS adapted from here: https://www.learnwithjason.dev/blog/get-form-values-as-json
 function handleFormSubmit(event) {
-    event.preventDefault();
-    
-    const data = new FormData(event.target);
-    
-    const formJSON = Object.fromEntries(data.entries());
+  event.preventDefault();
   
-    
-    const results = document.querySelector('.results pre');
-    results.innerText = JSON.stringify(formJSON, null, 2);
-  }
+  const data = new FormData(event.target);
   
-  const form = document.querySelector('.contact-form');
-  form.addEventListener('submit', handleFormSubmit);
+  const formJSON = Object.fromEntries(data.entries('color'));
+
+  // for multi-selects, we need special handling
+  // formJSON.colors = data.getAll('colors');
+  // formJSON.color = data.getAll('color');
+  // formJSON.snacks = data.getAll('snacks');
+  // formJSON.snacks = data.getAll('snacks');
+  
+  const results = document.querySelector('.results pre');
+  results.innerText = JSON.stringify(formJSON);
+}
+
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', handleFormSubmit);
