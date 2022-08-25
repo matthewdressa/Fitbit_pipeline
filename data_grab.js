9f5a2a56e78c534c94b4f3ebfd8ef0a0cbcfc193
Matthew Thomas Dressa <mtd67@cornell.edu>
2022-08-24 17:13:28 -0700

    Console log statements in js

--- data_grab.js
+++ data_grab.js
@@ -1,20 +1,42 @@
 // JS adapted from here: https://www.learnwithjason.dev/blog/get-form-values-as-json
+
+// let clock_pref = 'empty';
+var color_pref = 'empty';
+
+console.log(color_pref);
+// console.log(clock_pref);
+
 function handleFormSubmit(event) {
   event.preventDefault();
   
   const data = new FormData(event.target);
   
-  const formJSON = Object.fromEntries(data.entries('color'));
-
-  // for multi-selects, we need special handling
-  // formJSON.colors = data.getAll('colors');
-  // formJSON.color = data.getAll('color');
-  // formJSON.snacks = data.getAll('snacks');
-  // formJSON.snacks = data.getAll('snacks');
+  const formJSON = Object.fromEntries(data.entries());
   
+  // Saving the preferences of users which is grabbed from the json Object. 
+  color_pref = formJSON['color'];
+  clock_pref = formJSON['clock'];
+
+  console.log(color_pref);
+  console.log(clock_pref);
+
+
+
   const results = document.querySelector('.results pre');
-  results.innerText = JSON.stringify(formJSON);
+  results.innerText = JSON.stringify(formJSON, 2);
+  
 }
 
 const form = document.querySelector('.contact-form');
 form.addEventListener('submit', handleFormSubmit);
+
+console.log(color_pref);
+console.log(clock_pref);
+
+// function getVars (clock, color){
+//     let clk = clock_pref,
+//         clr = color_pref; 
+
+//         return { clk, clr };
+
+// }
