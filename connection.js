9f37ebcd284d91f69cf288ac35492f7753a93009
Matthew Thomas Dressa <mtd67@cornell.edu>
2022-08-29 14:12:25 -0700

    copy from data grab to connection

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 document.writeln("<script type='text/javascript' src='data_grab.js'></script>");
-var x = handleFormSubmit(event);
+var x = handleFormSubmit();
+
 function color_pref(x) {
         // Def: Compares user color pref to array and returns it
 
@@ -24,10 +25,35 @@ function background_color(svg_color) {
     
     // The class is updated to match the color passed into parameters
     let updated_styling = document.getElementById('.'+svg_color.toString()).className;
-    return 
+    return updated_styling;
 
+    //Daniel's attempt to set the background color
+    document.getElementById("background").className = svg_color;
+}
+
+function folder_zip (){
+    // TODO: This function should zip the folder root folder after changes have already een made 
+
+}
+
+function zip_download (){
+    // TODO: This function should download the function after it has already been zipped
 }
 
 
 const svg_color = color_pref(x);
 const ___ = background_color(svg_color);
+
+
+
+
+
+
+// Goals
+// 1. Grab variables input by users (still working on this)
+// 2. Create a function that specifies a user's color preference (example: yellow);
+// 3. Given this color preference ^, we want to change the background class of the svg tag located here                                                                               
+// 4. After that we want to zip the root folder
+// 5. Download zipped folder
+
+
