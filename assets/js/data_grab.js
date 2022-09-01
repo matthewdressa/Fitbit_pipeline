3c3b2ff6b3604047af038ad13c6ee0703c33399d
Matthew Thomas Dressa <mtd67@cornell.edu>
2022-09-01 13:07:16 -0700

    image_upload

--- assets/js/data_grab.js
+++ assets/js/data_grab.js
@@ -1,8 +1,8 @@
 // JS adapted from here: https://www.learnwithjason.dev/blog/get-form-values-as-json
 
 // let clock_pref = 'empty';
-var cp = 'empty';
-console.log(cp);
+// var cp = 'empty';
+// console.log(cp);
 // console.log(clock_pref);
 
 function handleFormSubmit(event) {
@@ -16,26 +16,37 @@ function handleFormSubmit(event) {
   const data = new FormData(event.target);
   
   const formJSON = Object.fromEntries(data.entries());
+
+
+
   
   // Saving the preferences of users which is grabbed from the json Object. Type is string
   var color_pref = formJSON['color'];
+  var file_name_ex = formJSON['filename'];
+
+  var file_name = formJSON['filename']['name'];
+
   // clock_pref = formJSON['clock'];
 
   // Checking this saved value
   console.log(color_pref);
+  console.log(file_name_ex);
+
+  console.log(file_name);
+
   // console.log(clock_pref);
 
 
 
   const results = document.querySelector('.results pre');
-  results.innerText = JSON.stringify(formJSON, 2);
+  results.innerText = JSON.stringify(formJSON, null, 2);
 
-  return color_pref;
+  // return color_pref;
   
 }
 
 // Gathers data in the form and puts it into a constant. Second line calls the above function
-const form = document.querySelector('.contact-form');
+const form = document.querySelector('.form');
 form.addEventListener('submit', handleFormSubmit);
 
 
