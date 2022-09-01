c176c54209168d424143ab5c7630cda7483c653f
Matthew Thomas Dressa <mtd67@cornell.edu>
2022-09-01 10:20:58 -0700

    updated_js

--- assets/js/data_grab.js
+++ assets/js/data_grab.js
@@ -1,19 +1,64 @@
+// JS adapted from here: https://www.learnwithjason.dev/blog/get-form-values-as-json
+
+// let clock_pref = 'empty';
+var cp = 'empty';
+console.log(cp);
+// console.log(clock_pref);
+
 function handleFormSubmit(event) {
-    event.preventDefault();
-    
-    const data = new FormData(event.target);
-    
-    const formJSON = Object.fromEntries(data.entries());
+  // Function definition: Handles the submitted form by saving all entries into
+  // a JSON object.User's color preference is extracted from the JSON object and 
+  // saved into the variable color_pref. Results are then displayed on the webpage
+
+  // Clears the users data 
+  event.preventDefault();
   
-    // for multi-selects, we need special handling
-    // formJSON.colors = data.getAll('colors');
-    formJSON.snacks = data.getAll('snacks');
-    // formJSON.snacks = data.getAll('snacks');
-    // formJSON.snacks = data.getAll('snacks');
-    
-    const results = document.querySelector('.results pre');
-    results.innerText = JSON.stringify(formJSON, null, 2);
-  }
+  const data = new FormData(event.target);
   
-  const form = document.querySelector('.contact-form');
-  form.addEventListener('submit', handleFormSubmit);
+  const formJSON = Object.fromEntries(data.entries());
+  
+  // Saving the preferences of users which is grabbed from the json Object. Type is string
+  var color_pref = formJSON['color'];
+  // clock_pref = formJSON['clock'];
+
+  // Checking this saved value
+  console.log(color_pref);
+  // console.log(clock_pref);
+
+
+
+  const results = document.querySelector('.results pre');
+  results.innerText = JSON.stringify(formJSON, 2);
+
+  return color_pref;
+  
+}
+
+// Gathers data in the form and puts it into a constant. Second line calls the above function
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', handleFormSubmit);
+
+
+
+
+
+
+// function handleFormSubmit(event) {
+//     event.preventDefault();
+    
+//     const data = new FormData(event.target);
+    
+//     const formJSON = Object.fromEntries(data.entries());
+  
+//     // for multi-selects, we need special handling
+//     // formJSON.colors = data.getAll('colors');
+//     formJSON.snacks = data.getAll('snacks');
+//     // formJSON.snacks = data.getAll('snacks');
+//     // formJSON.snacks = data.getAll('snacks');
+    
+//     const results = document.querySelector('.results pre');
+//     results.innerText = JSON.stringify(formJSON, null, 2);
+//   }
+  
+//   const form = document.querySelector('.contact-form');
+//   form.addEventListener('submit', handleFormSubmit);
